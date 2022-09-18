40d8c8c1ccbf761f04486edd78d3e5c2ddb099b8
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2022-09-17 20:30:27 -0400

    second commit

--- index.jsx
+++ index.jsx
@@ -1,10 +1,24 @@
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 const router = jsonServer.router("animeDB.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.use((err, req, res, next) => {
+   res.status(500).send({message: err.message});
+});
+
+server.get("/", (req, res) =>{
+   res.send("Server Is Ready");
+});
+
+//server port listening on/ serving on.//
+const port = process.env.PORT || 54441;
+server.listen(port, () => {
+    console.log(`Server Is Ready At http://localhost:${port}`);
+});
+
+module.exports = server;
